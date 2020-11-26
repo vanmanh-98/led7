d8ca61922dcdec3063aeee3d1423dec387231f55
= <=>
2020-11-26 09:35:17 +0700

    update

--- 2led7.c
+++ 2led7.c
@@ -8,28 +8,31 @@ char so[10]={0xFC,0x06,0xDA,0xF2,0x4C,0xB6,0xE0,0xFE,0xF6};
 void delay_ms(unsigned long time);
 void main()
 {
+	P2_0=1;
 	while(1)
 	{
-	P1=0;
-	LED1=bat;
-	LED2=tat;
-	P2=so[1];
-	delay_ms(100);
-	LED1=tat;
-	LED2=bat;
-	P2=so[2];
-	delay_ms(100);
+		P2_0=~P2_0;
+		delay_ms(1000);
+//		P1=0;
+//		LED1=bat;
+//		LED2=tat;
+//		P2=so[1];
+//		delay_ms(100);
+//		LED1=tat;
+//		LED2=bat;
+//		P2=so[2];
+//		delay_ms(100);
 	}
 }
 void delay_ms(unsigned long time)
 {
-	TMOD=0x01;
+	TMOD=0x01; 
 	while(time!=0)
 	{
-		TL0=0x18;
-		TH0=0xFC;
+		TL0=(65535 - 333)%256;
+		TH0=(65535 - 333)/256;
 		TR0=1;
-		while(TF0!=0){}
+		while(TF0==0);
 		TR0=0;
 		TF0=0;
 		time--;
